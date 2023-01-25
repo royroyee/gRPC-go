5dbbded871d1a69f55754bacf952659c858697aa
HWAN <kim131325@naver.com>
2023-01-26 00:32:33 +0900

    Create gRPC-server (helloworld)

---
+++ ex1/gRPC-server/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	pb "github.com/royroyee/gRPC-go/gRPC-server/protos/helloworld"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server:%v", err)
+	}
+}
